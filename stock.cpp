5fe4201309f5610d857068dc3744779a8b4062d8
Cheng Ho Ming <eric310@connect.hku.hk>
2024-03-28 19:26:29 +0800

    Draft price history functions and Event modifier functions
    We use a dynamic array for price history, and a linked list of event modifiers. Since price history array is programmed in this commit, we add the delta_price() delta_price_percentage() functions as well.

--- stock.cpp
+++ stock.cpp
@@ -43,6 +43,94 @@ unsigned int Stock::num_stocks_affordable(double balance) {
     return (unsigned int) balance / price;
 }
 
+void Stock::initialize_history(void) {
+    // Initialize the history array
+    history_array_size = 10;
+    history_index = 0;
+    double * history = new double[history_array_size];
+    history[0] = price;
+}
+
+void Stock::update_history(void) {
+    if (history == nullptr) {
+        // If the history array is not initialized, throw an error
+        initialize_history();
+    }
+    // Update the history array with the current price
+    if (history_index == history_array_size) {
+        // If the history array is full, double the size
+        double * new_history = new double[history_array_size * 2];
+        for (int i = 0; i < history_array_size; i++) {
+            new_history[i] = history[i];
+        }
+        delete[] history;
+        history = new_history;
+        // A general practice is to double the array size
+        history_array_size *= 2;
+    }
+    history[history_index] = price;
+    history_index++;
+}
+
+void Stock::delete_memory(void) {
+    // Delete the dynamically allocated memory
+    delete[] history;
+    history = nullptr; // Set the pointer to null
+    // This is a good practice to avoid using a dangling pointer
+    // In case someone forgot delete the stock object
+    
+    // TODO: delete the event modifier linked list
+}
+
+double Stock::delta_price(void) {
+    // Return the change of stock price
+    // which stock prices are stored in the history array
+    if (history_index < 2) {
+        // If there are less than two prices in the history array, return 0
+        return 0;
+    }
+    // Return the change of the last two prices
+    return history[history_index - 1] - history[history_index - 2];
+}
+
+double Stock::delta_price_percentage(void) {
+    // Return the percentage of change of stock price using Stock::delta_price();
+    if (history_index < 2) {
+        // If there are less than two prices in the history array, return 0
+        return 0;
+        // since history[history_index - 2] will return strange value
+    }
+    return delta_price() / history[history_index - 2];
+}
+
+void Stock::add_event(Stock_event event) {
+    // Add an event to the stock
+    // Create a new event modifier and add it to the linked list
+    if (event_modifier_head == nullptr) {
+        // If the linked list is empty, create a new event modifier
+        Event_Modifier * new_event = new Event_Modifier;
+        new_event->sd_change = event.sd;
+        new_event->skew_change = event.skew;
+        new_event->duration = event.duration;
+        new_event->next = nullptr;
+        event_modifier_head = new_event;
+        return;
+    }
+    else {
+        // If the linked list is not empty, find the last element
+        Event_Modifier * current = event_modifier_head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        // Create a new event modifier and add it to the end of the linked list
+        Event_Modifier * new_event = new Event_Modifier;
+        new_event->sd_change = event.sd;
+        new_event->skew_change = event.skew;
+        new_event->duration = event.duration;
+        new_event->next = nullptr;
+        current->next = new_event;
+    }
+}
 
 void Stock::init(void) {
     // Assign a random price, standard deviation, skewness, and category to the stock
--- stock.h
+++ stock.h
@@ -2,7 +2,9 @@
 #define STOCK_H
 
 #include <string>
+#include "events.h"
 using namespace std;
+
 class Stock {
     public:
         double purchase(double &balance, unsigned int amount);
@@ -10,10 +12,12 @@ class Stock {
         unsigned int num_stocks_affordable(double balance);
         void init(void);
         string category_name(void);
-        // todo: double delta_price(void) function
-        // todo: double delta_price_percentage(void) function
+        void delete_memory(void);
+        double delta_price(void);
+        double delta_price_percentage(void);
         // todo: void next_round(void) function
         // todo: void event_modifier(double sd, double skew, unsigned int duration) function
+
     private:
         string name;
         double price;
@@ -21,14 +25,29 @@ class Stock {
         double sd; // standard deviation
         double skew; // skewness
         unsigned int category; // category names in names.h
-        // todo: stock history using linked list
-        // todo: event offsets & duration using linked list
-        /* e.g.
-        [[sd_change_1, skew_change_1, duration_1],
-         [sd_change_2, skew_change_2, duration_2], ...]
+
+        // dynamic array storing history
+        // since we don't need to delete history
+        // a linked list is not necessary
+        int history_array_size = 10;
+        double * history = new double[history_array_size];
+        int history_index = 0; // index of the next element to be added
+        void initialize_history(void);
+        void update_history(void);
+        /* TODO: Summarize the above code to README.md:
+        "Dynamic memory management (e.g., dynamic arrays, linked lists, STL containers)"
         */
-        // In case multiple events apply modifiers to the same stock
-        
+
+        struct Event_Modifier {
+            double sd_change;
+            double skew_change;
+            unsigned int duration;
+            Event_Modifier * next;
+        };
+
+        Event_Modifier * event_modifier_head = nullptr;
+        void add_event(Stock_event event);
+
 };
 
 #endif
