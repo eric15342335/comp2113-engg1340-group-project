a2323e7e0501f3c385b726943d093cad88c2a747
Cheng Ho Ming <eric310@connect.hku.hk>
2024-04-06 17:32:43 +0800

    formatting

--- printing.cpp
+++ printing.cpp
@@ -6,14 +6,13 @@ void replace_screen_line(vector<string> & contents, string new_content, int line
     // Given a vector of strings, replace the line-th line with new_content
     // And then print the vector to cout
     // first clean the screen
-    for (int i = 0; i < contents.size(); i++){
+    for (int i = 0; i < contents.size(); i++) {
         cout << "\033[2J\033[1;1H"; // clear screen
     }
     contents[line] = new_content;
     print_vector(contents);
     return;
 }
-
 
 void print_vector(vector<string> contents) {
     // Print the vector to cout
