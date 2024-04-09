36c3b4fd6f955883fbb28ad24ad35483695b1fcd
Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>
2024-04-09 15:31:32 +0800

    Update random_price.h

--- random_price.h
+++ random_price.h
@@ -2,31 +2,31 @@
 #define RANDOM_PRICE_H
 
 /**
-#ifndef 
+#ifndef
 #define are to prevent double include the header
 */
 /**
-* Initial Stock Price Generator
-* if a=2 mean 50 sd 20; if a=3 mean 150 sd 50; else mean 5 sd 2;
-*Stock prices cluster in 3 tiers in our world: about 5 hkd (a=ELSE) variating about 2 hkd; , 50 hkd(a=2) variating about 20 hkd, 
-*and 150 hkd(a=3)variating about 50 hkd (based on a very little sample observation of real world). 
-*/
+ * Initial Stock Price Generator
+ * if a=2 mean 50 sd 20; if a=3 mean 150 sd 50; else mean 5 sd 2;
+ *Stock prices cluster in 3 tiers in our world: about 5 hkd (a=ELSE) variating about 2 hkd; , 50 hkd(a=2) variating about 20 hkd,
+ *and 150 hkd(a=3)variating about 50 hkd (based on a very little sample observation of real world).
+ */
 double init_stock_price(int a);
 /**
-* Stock Standard Deviation Generator (for both initial and after initial)
-* s.d. usually 1%-5% of initial stock price; 
-* modelling with absolute value of norm dist mean 0.03*init_price; s.d. 0.02*init_price (yes, s.d. of s.d.)
-*/
+ * Stock Standard Deviation Generator (for both initial and after initial)
+ * s.d. usually 1%-5% of initial stock price;
+ * modelling with absolute value of norm dist mean 0.03*init_price; s.d. 0.02*init_price (yes, s.d. of s.d.)
+ */
 double random_sd(float init_price);
 /**
-* Random Stock Price Generator (non-initialising)
-* requires mean, s.d., upper limit increase threshold (FRACTION of mean) and lower limit decrease threshold (FRACTION of mean)
-* e.g. both 0.03; then the mean changes to exactly the new price if the value differ from mean by 3%
-*/
-double random_stock(float &mean, float &sd,float &upper_limit, float &lower_limit);
+ * Random Stock Price Generator (non-initialising)
+ * requires mean, s.d., upper limit increase threshold (FRACTION of mean) and lower limit decrease threshold (FRACTION of mean)
+ * e.g. both 0.03; then the mean changes to exactly the new price if the value differ from mean by 3%
+ */
+double random_stock(float & mean, float & sd, float & upper_limit, float & lower_limit);
 /**
-* Random integer function in choosing stock type
-* Return a random integer from 0 to max - 1
-*/
+ * Random integer function in choosing stock type
+ * Return a random integer from 0 to max - 1
+ */
 unsigned int random_integer(unsigned int max_integer);
 #endif
