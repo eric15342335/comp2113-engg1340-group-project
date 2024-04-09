711ed95988da3698a1a34a35e335fe8135ca729d
Chung Shing Hei <70740754+MaxChungsh@users.noreply.github.com>
2024-04-09 20:52:26 +0800

    Update names.h

--- names.h
+++ names.h
@@ -2,16 +2,18 @@
 #define NAMES_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
 // the index map to the names, e.g. category = 0 is tech
 // we will decide what category we will include later
-const unsigned int category_list_size = 5;
+const int category_list_size = 19;
 // we use extern since we will define the array in names.cpp
 // we don't want to define the array in names.h since it's not a good practice
 // and will cause multiple definition error
 extern string category_list[category_list_size];
 
-string generate_name(unsigned int category);
+vector<string> generate_name(unsigned int category, int num);
+
 
 #endif
