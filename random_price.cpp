14fba8aae06e8b6b758b58b792063aab079bf30d
Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>
2024-04-09 16:04:24 +0800

    First working demo of random price generation (#10)
    * Update random_price.cpp
    
    * Update random_price.h
    
    * Update random_price.cpp
    
    * Comment migration
    
    Most comments from cpp are migrated here to the header.
    
    * Fixed bug of invalid s.d; also migrated to use std:: instead of using namespace sts.
    
    Note the comments are migrated to the header.
    
    * Update random_price.cpp
    
    * Update random_price.cpp
    
    * should pass clang formatting now
    
    * Update random_price.cpp
    
    * Update random_price.h
    
    * Update random_price.cpp
    
    * Update random_price.cpp (var name
    
    * Update random_price.h

--- random_price.cpp
+++ random_price.cpp
@@ -1,9 +1,44 @@
 // code related to randomness
-#include <cstdlib>
 #include "random_price.h"
-using namespace std;
+#include <cstdlib>
+#include <iostream>
+#include <random>
 
+double init_stock_price(int price_profile) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<double> distribution(5.0, 2.0);
+    if (price_profile == 2) {
+        distribution.param(std::normal_distribution<double>::param_type(50.0, 20.0));
+    }
+    if (price_profile == 3) {
+        distribution.param(std::normal_distribution<double>::param_type(150.0, 50.0));
+    }
+    return distribution(gen);
+}
+double random_sd(float price) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<double> d(0.03 * price, 0.02 * price);
+    return abs(d(gen));
+}
+double random_stock(float & mean, float & sd, float & upper_limit,
+                    float & lower_limit) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<double> distribution(mean, sd);
+    float price = distribution(gen);
+    // for mean > temp i.e. falling prices
+    if ((mean - price) > (lower_limit * mean)) {
+        mean = price;
+    }
+    // rising prices
+    if ((price - mean) > (upper_limit * mean)) {
+        mean = price;
+    }
+    sd = random_sd(price);
+    return price;
+}
 unsigned int random_integer(unsigned int max_integer) {
-    // Return a random integer from 0 to max - 1
     return rand() % max_integer;
 }
