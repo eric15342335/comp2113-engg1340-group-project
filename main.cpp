a848c82f2d03cb102c497bedff3da7357639c4f0
Cheng Ho Ming <eric310@connect.hku.hk>
2024-04-12 17:51:51 +0800

    Add test case for table and stock

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 /**
  * @file main.cpp
  */
-
-#include <iostream>
-#include <list>
-#include <vector>
-#include <string>
 #include "stock.h"
+#include "player.h"
+#include "random_price.h"
+#include "nonstdlibs/VariadicTable.h"
 
 /**
  * 1% trading fees
@@ -16,24 +14,65 @@
 const float trading_fees_percent = 0.01;
 
 /** Initial stock count */
-const int initial_stock_count = 10;
+const int initial_stock_count = 20;
 
-float player_balance = 1000;
+void print_table(std::vector<Stock> stocks_list, Player player) {
+    VariadicTable<unsigned int, std::string, float, float, float, unsigned int, float, unsigned int>
+        table({"No.", "Name", "Last Price", "Change", "\% Change", "Quantity", "$ Spent", "Max"});
+
+    table.setColumnPrecision({1, 0, 2, 1, 1, 1, 2, 1});
+    table.setColumnFormat({
+        VariadicTableColumnFormat::AUTO,
+        VariadicTableColumnFormat::AUTO,
+        VariadicTableColumnFormat::FIXED,
+        VariadicTableColumnFormat::FIXED,
+        VariadicTableColumnFormat::FIXED,
+        VariadicTableColumnFormat::AUTO,
+        VariadicTableColumnFormat::FIXED,
+        VariadicTableColumnFormat::AUTO,
+    });
+    for (unsigned int i = 0; i < stocks_list.size(); i++) {
+        table.addRow(i + 1, stocks_list[i].get_name(), stocks_list[i].get_price(),
+                     stocks_list[i].delta_price(), stocks_list[i].delta_price_percentage() * 100,
+                     stocks_list[i].get_quantity(), stocks_list[i].get_money_spent(),
+                     stocks_list[i].num_stocks_affordable(player.bal(), trading_fees_percent));
+    }
+    table.print(std::cout);
+}
 
 /** Main function */
 int main(void) {
-    std::list<Stock> stocks_list;
+    std::vector<Stock> stocks_list;
+    Player player;
+    player.start_game();
     for (int i = 0; i < initial_stock_count; i++) {
         Stock stock;
         stock.init();
         stocks_list.push_back(stock);
     }
 
-    std::cout << "Welcome to the stock market game!" << std::endl;
-    for (Stock stock : stocks_list) {
-        std::cout << stock.get_name() << " " << stock.get_price()
-                  << " " << stock.get_quantity() << " " << stock.get_money_spent() << std::endl;
+    std::cout << "Welcome to the Stock Market Simulator!" << std::endl;
+    std::cout << "Current trading fees are charged at " << trading_fees_percent * 100 << " %" << std::endl;
+    std::cout << "You currently have $" << *player.get_balance_ptr() << "." << std::endl;
+    /** Test for VariadicTable.h */
+
+    print_table(stocks_list, player);
+    /** Simulate player buying and selling stocks automatically */
+    for (unsigned int i = 0; i < stocks_list.size(); i++) {
+        int num_buyable = stocks_list[i].num_stocks_affordable(player.bal(), trading_fees_percent);
+        if (num_buyable > 0) {
+            stocks_list[i].purchase(*player.get_balance_ptr(), random_integer(num_buyable), trading_fees_percent);
+        }
     }
+    /** Go to next round */
+    player.next_round();
+    std::cout << "Played: " << player.get_rounds_played() << " Rounds." << std::endl;
+    for (unsigned int i = 0; i < stocks_list.size(); i++) {
+        stocks_list[i].next_round();
+    }
+    player.next_round();
+
+    print_table(stocks_list, player);
 
     /**
      * todo: implement UI code
