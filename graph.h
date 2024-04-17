74cd2a8db997d10cd9f89cf6c8683b4f5c0c443c
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-04-17 23:00:50 +0800

    Docs: Various improvements on documentations and comments (#24)
    * Improve in-code documentations
    
    * Docs: Add doxygen configuration file

--- graph.h
+++ graph.h
@@ -1,8 +1,10 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 #include <string>
+
+/** Prints the graph of the stock price history to `std::cout`.
+ * @param stockname Name of the stock.
+ */
 void graph_plotting(std::string stockname);
-// input: stockname + stockname.log file
-// return none
-// output: print the graph of the stock price history
+
 #endif
