dee42e5eb8e132963cbd6291ce5dae107f3d7b2e
Comet <67854955+comet13579@users.noreply.github.com>
2024-04-19 02:47:39 +0800

    Variable Graph Size (#27)
    * Add files via upload
    
    * add variable height and width of the graph
    
    * minor fixes and clang-format
    
    * clang-format
    
    * clang-format header
    
    * enter the commit message for your changes. Lines starting
    
    * remove sussy stuff
    
    * minor fix
    
    * ui for misc information
    
    * clang
    
    ---------
    
    Co-authored-by: Prismatiscence <56928422+Prismatiscence@users.noreply.github.com>
    Co-authored-by: Prismatiscence <chowyuihei@gmail.com>

---
+++ draw.h
@@ -0,0 +1,9 @@
+#ifndef DRAW_H
+#define DRAW_H
+
+void drawRoundInfo(int row, int col, int round, float balance);
+void drawEventBar(int row, int col);
+void listEvents(int row, int col);
+void drawButton(int row, int col);
+
+#endif
