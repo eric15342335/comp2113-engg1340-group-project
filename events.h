fbac479a2fa4c5c3ef61d69fc20448f0cd51c0e0
Chung Shing Hei <70740754+MaxChungsh@users.noreply.github.com>
2024-04-19 00:26:19 +0800

    Update events.h
    Signed-off-by: Chung Shing Hei <70740754+MaxChungsh@users.noreply.github.com>

--- events.h
+++ events.h
@@ -55,7 +55,6 @@ struct Stock_event {
         /** The type of event: Apply to all stocks, in one category or randomly? */
         event_type type_of_event;
 
-        /** If this event is Stock categories names are hardcoded in events.h */
         unsigned int category;
 
         /** Stores the stock_modifiers that the event applies. */
