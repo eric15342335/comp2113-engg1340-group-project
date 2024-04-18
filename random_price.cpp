0d12bb81cdde9fd3abb84691f4584e84d6e10274
Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>
2024-04-18 14:45:48 +0800

    make should work now

--- random_price.cpp
+++ random_price.cpp
@@ -39,8 +39,8 @@ float init_sd(int price_profile) {
 float percentage_change_price(Stock & stock) {
     float current_price = stock.return_most_recent_history(1)[0];
     float init_price = stock.return_most_recent_history(99999999)[0];
-    float mean = stock.get_attribute(mean) + stock.sum_attribute(mean);
-    float standard_deviation = stock.get_attribute(standard_deviation) + stock.sum_attribute(standard_deviation);
+    float offset = stock.get_attribute(mean) + stock.sum_attribute(mean);
+    float sd = stock.get_attribute(standard_deviation) + stock.sum_attribute(standard_deviation);
     unsigned int rounds_passed = stock.get_history_size();
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -50,8 +50,8 @@ float percentage_change_price(Stock & stock) {
     float temp = 100 * abs(init_price - current_price) / init_price;
     float upper_limit = 50 + (rounds_passed * rounds_passed) / 15 + temp;
     float lower_limit = -1 * (50 + rounds_passed + temp);
-    float z_score_upper_limit = (upper_limit - mean) / standard_deviation;
-    float z_score_lower_limit = (lower_limit - mean) / standard_deviation;
+    float z_score_upper_limit = (upper_limit - offset) / sd;
+    float z_score_lower_limit = (lower_limit - offset) / sd;
     for (int i = 0; i < 5; i++) {
         if (z_score_lower_limit > 1.3) {
             stock.change_mean(current_price * 0.3);
@@ -60,7 +60,7 @@ float percentage_change_price(Stock & stock) {
             stock.change_mean(current_price * -0.3);
         }
     }
-    std::normal_distribution<float> distribution(mean, standard_deviation);
+    std::normal_distribution<float> distribution(offset, sd);
 
     if (lower_limit < -100) {
         lower_limit = -100;
