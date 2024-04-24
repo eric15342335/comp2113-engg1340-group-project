da14dc09b784e254e859afb0816a22d50d4f1f77
Prismatiscence <56928422+Prismatiscence@users.noreply.github.com>
2024-04-24 19:00:07 +0800

    UI: Basic User Input Stuff (#49)
    * make console input actually useful stage 1
    
    * create placeholder buy and sell functions
    
    * misc stuff
    
    * minor changes to controls.cpp
    
    * Fix failing graph test case
    
    * Run clang-format
    
    make fix
    
    * Run clang-format
    
    make fix
    
    * Failthrough switch-case
    
    * ui format fix attempt 2
    
    * pointer fix
    
    * pointer fix
    
    * added simple testcase
    
    * clang
    
    ---------

--- draw.cpp
+++ draw.cpp
@@ -89,15 +89,6 @@ void drawButton(int row, int col) {
     buttons = options.size();
     width = (int)(col / buttons);
 
-    /*
-    std::cout << setCursorPosition(row - 2, 3);
-    for (int i = 0; i < buttons; i++) {
-        i % 2 == 0 ? std::cout << textBlack << bgWhite : std::cout << textWhite <<
-    bgBlack; for (int j = 0; j < width; j++) { std::cout << " ";
-        }
-    }
-    */
-
     std::cout << textReset << setCursorPosition(row - 1, 3);
     for (int i = 0; i < buttons; i++) {
         i % 2 == 0 ? std::cout << bgWhite << textBlack
@@ -112,13 +103,5 @@ void drawButton(int row, int col) {
             std::cout << " ";
         }
     }
-    /*
-    std::cout << textReset << setCursorPosition(row, 3);
-    for (int i = 0; i < buttons; i++) {
-        i % 2 == 0 ? std::cout << textBlack << bgWhite : std::cout << textWhite <<
-    bgBlack; for (int j = 0; j < width; j++) { std::cout << " ";
-        }
-    }
-    */
     std::cout << textReset << "\n";
 }
