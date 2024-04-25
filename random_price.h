c2bbea086383944e247c2e7a0275dbb37133ec09
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-04-25 17:37:28 +0800

    Table: Fix incorrectly displayed stock_modifiers value (#66)
    * Fix incorrectly displayed stock_modifiers value
    
    Co-Authored-By: Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>
    
    * Run clang-format
    
    make fix
    
    ---------
    
    Co-authored-by: Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>

--- random_price.h
+++ random_price.h
@@ -7,13 +7,26 @@
 #ifndef RANDOM_PRICE_H
 #define RANDOM_PRICE_H
 
+#include "events.h"
 #include "stock.h"
 
 /// @brief Multiplier for mean
-const float meanMultiplier = 0.0007;
+const float meanMultiplier = 0.05;
 
 /// @brief Multiplier for standard deviation
 const float sdMultiplier = 3.0;
+
+/// @brief Lower limit multiplier
+const float lowerLimitMultiplier = 0.2;
+
+/// @brief Upper limit multiplier
+const float upperLimitMultiplier = 0.2;
+
+/// @brief Default lower limit
+const float defaultLowerLimit = -5 / lowerLimitMultiplier;
+
+/// @brief Default upper limit
+const float defaultUpperLimit = 5 / upperLimitMultiplier;
 
 /**
  * @brief Initialize starting stock price.
@@ -32,6 +45,15 @@ float init_stock_price(int a);
  * @brief Initialize the standard deviation of the stock price
  */
 float init_sd(void);
+
+/**
+ * @brief Get the processed modifiers for the stock.
+ * @param stock the stock to get the processed modifiers
+ * @return the processed modifiers
+ * @note the values of the processed modifiers are final values that will be used to
+ * calculate the percentage change of the stock price
+ */
+std::map<stock_modifiers, float> getProcessedModifiers(Stock & stock);
 
 /**
  * @brief Calculate the percentage change of the stock price.
