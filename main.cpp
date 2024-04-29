2095d7e6b548809558bfee3c28daf2970c55cfc5
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-04-29 23:01:22 +0800

    UI: Enable Windows 10 Support on ANSI Escape Codes

--- main.cpp
+++ main.cpp
@@ -20,8 +20,18 @@
 
 #ifdef _WIN32
 #include <windows.h>
+void enableWindowsVTProcessing(void) {
+    // Set the console to UTF-8 mode
+    SetConsoleOutputCP(65001);
+    // Get the current console mode
+    DWORD consoleMode;
+    GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &consoleMode);
+    // Enable virtual terminal processing
+    consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), consoleMode);
+}
 #else
-#define SetConsoleOutputCP(x) // Do nothing
+#define enableWindowsVTProcessing() // Do nothing
 #endif
 
 /**
@@ -255,8 +265,8 @@ void next_round_routine(
 
 /** Main function, the entry point of the program */
 int main(void) {
-    // Set the console to UTF-8 mode
-    SetConsoleOutputCP(65001);
+    enableWindowsVTProcessing();
+
     bool advance;      // Whether to advance to the next round
     bool gameQuit = 0; // Whether the player wants to quit the game
     bool viewMode = 0; // 0 to view table, 1 to view graph
