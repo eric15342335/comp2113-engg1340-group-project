c1bdc43c207396bd93295afbd315537ebab0fb4b
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-04-29 23:29:17 +0800

    Minor fixes for PR #87

--- main.cpp
+++ main.cpp
@@ -292,12 +292,12 @@ int main(void) {
     drawLogo(row, col);
     time::sleep(sleepMedium);
 
-    std::cout << "Please enter.\n0 for new save,\n1 for loading old save,\n2 "
+    std::cout << "Please enter.\n0 for new save,\n1 for loading old save(s),\n2 "
                  "for deleting save,\n3 to quit: ";
     std::cin >> loadsave;
     while (loadsave != "0" && loadsave != "1" && loadsave != "2" && loadsave != "3") {
         std::cout << "Invalid input.\nPlease enter.\n0 for new save,\n1 for loading "
-                     "old save,\n2 for deleting save,\n3 to quit:"
+                     "old save(s),\n2 for deleting save,\n3 to quit:"
                   << std::endl;
         std::cin >> loadsave; // choose new file or load previous file
     }
