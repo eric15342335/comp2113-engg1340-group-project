1543c213aabad190fdc0d49c7bbf5ebf87df1c6d
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-06-02 17:28:28 +0800

    Names: Fix clang-format in header file

--- include/names.h
+++ include/names.h
@@ -41,8 +41,8 @@ std::vector<std::string> generate_name(unsigned int category, unsigned int num);
  * @param num The number of company names to generate.
  * @param words A vector of words to use in the company names.
  * @param suffixes A vector of suffixes to use in the company names.
- * @param companyNames The vector of company names to generate. This vector will be modified
- * by the function.
+ * @param companyNames The vector of company names to generate. This vector will be
+ * modified by the function.
  */
 void pickUniqueNames(const unsigned int & num, const std::vector<std::string> & words,
     const std::vector<std::string> & suffixes, std::vector<std::string> & companyNames);
